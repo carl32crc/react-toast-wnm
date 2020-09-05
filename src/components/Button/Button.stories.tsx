003efd73dfe66b9747abee2b7391da614d45b762
Carlos Ruiz <carl.45.cr@gmail.com>
2020-09-05 12:00:03 +0200

    update README.md

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { Button } from './Button';
+import { useToast } from 'react-toast-wnm';
 
 export default {
   title: 'Button',
 };
 
 export const Sample = (): JSX.Element => <Button>Button Label</Button>;
+export const MySample = (): JSX.Element => {
+  const toast = useToast();
+  return (
+    <Button
+      onClick={() => {
+        toast({
+          type: 'success',
+        });
+      }}
+    >
+      Button Label
+    </Button>
+  );
+};
